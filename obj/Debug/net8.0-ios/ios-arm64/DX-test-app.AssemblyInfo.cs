00e8322f35a9fb50f2181dfbcf23885911c1e060
Corne <corne.ackerman1104@gmail.com>
2024-04-03 15:27:22 +0200

    Removed Converter

--- obj/Debug/net8.0-ios/ios-arm64/DX-test-app.AssemblyInfo.cs
+++ obj/Debug/net8.0-ios/ios-arm64/DX-test-app.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DX-test-app")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+34cc6e1a6ab7df702ca3a55dc3604c247eb2f4e3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+1c736469b5d061383df595a393b1fea9367d7c0b")]
 [assembly: System.Reflection.AssemblyProductAttribute("DX-test-app")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DX-test-app")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
